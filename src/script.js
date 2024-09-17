939a75291af02f0f6b3e110951ddb7300c2c7326
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2024-09-17 09:40:40 +0100

    fixed navbar issues on mobile

--- src/script.js
+++ src/script.js
@@ -1,24 +1,40 @@
-var mobileNav = document.querySelector(".mobile-nav");
-var faqs = document.querySelectorAll(".faq");
+var mobileNav = document.querySelector(".mobile-nav")
+var faqs = document.querySelectorAll(".faq")
+var navlinks = document.querySelector(".navlinks")
+var services = document.querySelector(".services-navlink")
+let dropdown = document.querySelector('.services-dropdown');
 
 mobileNav.addEventListener('click', () => {
-    let navState = mobileNav.getAttribute("data-state");
+    let navState = mobileNav.getAttribute("data-state")
     if (navState == "expanded") {
-        mobileNav.setAttribute("data-state", "collapsed");
-        console.log(mobileNav.firstChild);
-        mobileNav.children[1].style.display = "none";
-        mobileNav.children[0].style.display = "Block";
+        mobileNav.setAttribute("data-state", "collapsed")
+        mobileNav.children[1].style.display = "none"
+        mobileNav.children[0].style.display = "Block"
+        navlinks.style.display = "none"
     } else {
-        mobileNav.setAttribute("data-state", "expanded");
-        mobileNav.children[1].style.display = "block";
-        mobileNav.children[0].style.display = "none";
+        mobileNav.setAttribute("data-state", "expanded")
+        mobileNav.children[1].style.display = "block"
+        mobileNav.children[0].style.display = "none"
+        // close services dropdown
+        dropdown.setAttribute("data-state", "collapsed")
+        navlinks.style.display = "flex"
     }
 })
 
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
-        let active = document.querySelector('.active');
-        active.classList.remove("active");
-        faq.classList.add("active");
+        let active = document.querySelector('.active')
+        active.classList.remove("active")
+        faq.classList.add("active")
     })
+})
+
+services.addEventListener('click', (e) => {
+    e.preventDefault()
+    let dropdownState = dropdown.getAttribute("data-state")
+    if (dropdownState == "expanded") {
+        dropdown.setAttribute("data-state", "collapsed")
+    } else {
+        dropdown.setAttribute("data-state", "expanded")
+    }
 })
